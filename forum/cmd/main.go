fb0b84d2893f0c1e6ea4ddb9776e2d24e28dd6fa
Adil-9 <96561291+Adil-9@users.noreply.github.com>
2023-04-16 01:14:23 +0600

    Add files via upload

--- forum/cmd/main.go
+++ forum/cmd/main.go
@@ -13,22 +13,32 @@ import (
 )
 
 func main() {
-	db, err := repository.OpenSqliteDB("store.db")
-	if err != nil {
-		log.Fatalf("error while opening db: %s", err)
-	}
-
 	addr := flag.String("addr", ":8000", "HTTP network address")
 	flag.Parse()
 
-	repository := repository.NewRepository(db)
-	service := service.NewService(repository)
-	handler := delivery.NewHandler(service)
-	server := new(server.Server)
+	handler, server, err := manage()
+	if err != nil {
+		return
+	}
 
 	fmt.Printf("Starting server at addr %s\nhttp://localhost%s/\n", *addr, *addr)
 
 	if err := server.Run(*addr, handler.InitRoutes()); err != nil {
 		log.Fatalf("error while running the server: %s", err.Error())
 	}
+}
+
+func manage() (*delivery.Handler, *server.Server, error) {
+	db, err := repository.OpenSqliteDB("store.db")
+	if err != nil {
+		log.Fatalf("error while opening db: %s", err)
+		return nil, nil, err
+	}
+
+	repository := repository.NewRepository(db)
+	service := service.NewService(repository)
+	handler := delivery.NewHandler(service)
+	server := new(server.Server)
+
+	return handler, server, nil
 }
